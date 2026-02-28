0530db372a7c58e967186ea790f4943bbf675680
vdengine <84008246+vdengine@users.noreply.github.com>
2026-02-28 20:31:49 +0800

    Update starfield.js

--- assets/js/starfield.js
+++ assets/js/starfield.js
@@ -1,1 +1,42 @@
 
+const canvas = document.getElementById('stars');
+const ctx = canvas.getContext('2d');
+
+let width, height, stars = [];
+
+function init() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    canvas.width = width;
+    canvas.height = height;
+    
+    stars = [];
+    for (let i = 0; i < 200; i++) {
+        stars.push({
+            x: Math.random() * width,
+            y: Math.random() * height,
+            size: Math.random() * 2,
+            speed: Math.random() * 0.5
+        });
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = '#ffffff';
+    
+    stars.forEach(star => {
+        ctx.beginPath();
+        ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
+        ctx.fill();
+        
+        star.y += star.speed;
+        if (star.y > height) star.y = 0;
+    });
+    
+    requestAnimationFrame(animate);
+}
+
+window.addEventListener('resize', init);
+init();
+animate();
