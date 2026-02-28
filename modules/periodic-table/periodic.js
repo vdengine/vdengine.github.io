bbc4f0ac82e4e121eaafe4372443fc9bbedca578
vdengine <84008246+vdengine@users.noreply.github.com>
2026-02-28 20:09:00 +0800

    Delete modules directory

--- modules/periodic-table/periodic.js
+++
@@ -1,9 +0,0 @@
-for (let i = 0; i < 50; i++) {
-  const box = document.createElement("div");
-  box.style.width = "40px";
-  box.style.height = "40px";
-  box.style.background = "#66ccff";
-  box.style.display = "inline-block";
-  box.style.margin = "4px";
-  document.body.appendChild(box);
-}
