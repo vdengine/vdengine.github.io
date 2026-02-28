02eae3c99bda017f5d9a95400fe8e5cec97857e1
vdengine <84008246+vdengine@users.noreply.github.com>
2026-02-28 21:52:35 +0800

    Update elements.js

--- modules/periodic-table/elements.js
+++ modules/periodic-table/elements.js
@@ -1,1 +1,16 @@
-
+export const elements = [
+  {
+    number: 1,
+    symbol: "H",
+    name: "Hydrogen",
+    atomicMass: 1.008,
+    category: "nonmetal"
+  },
+  {
+    number: 2,
+    symbol: "He",
+    name: "Helium",
+    atomicMass: 4.0026,
+    category: "noble-gas"
+  }
+];
