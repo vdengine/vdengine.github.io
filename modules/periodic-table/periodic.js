d100a47fff2d3ced21434986699b90ff12b98cc7
vdengine <84008246+vdengine@users.noreply.github.com>
2026-02-28 18:54:08 +0800

    Update periodic.js

--- modules/periodic-table/periodic.js
+++ modules/periodic-table/periodic.js
@@ -1,1 +1,6 @@
 console.log("PERIODIC JS RUNNING");
+const test = document.createElement("div");
+test.style.width = "100px";
+test.style.height = "100px";
+test.style.background = "red";
+document.body.appendChild(test);
